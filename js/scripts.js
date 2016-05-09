2dabfe1e3cdf80c0b8d04372dcc98ab74994992a
npeters10 <npeters10@engr-131-04.eastern.ewu.edu>
2016-05-09 16:11:25 -0700

    Version 1

---
+++ js/scripts.js
@@ -0,0 +1,24 @@
+// Docs at http://simpleweatherjs.com
+$(function() {
+  $.simpleWeather({
+    location: 'Spokane, WA',
+    unit: 'f',
+      
+      
+    success: function(weather) {
+        
+        $('.city').text(weather.city);
+        $('.temp').text(weather.temp);
+        $('.cond-code').text(weather.code);
+        
+        console.log(weather);
+    },
+ 
+
+error: function(error) {
+    console.log('Error no weather');
+}
+                  
+});
+});
+
